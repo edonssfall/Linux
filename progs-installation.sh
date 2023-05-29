7a20fb6114e0fca9845abbc7de37b36709c2ce22
Leonid Tsarov <63953204+edonssfall@users.noreply.github.com>
2023-05-29 11:27:05 +0200

    add progs-installation.sh

---
+++ progs-installation.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Install Telegram-Desktop
+sudo apt-get update
+sudo apt install telegram-desktop -y
+
+# Install GIT
+sudo apt-get update
+sudo apt install git -y
+
+# Install Synaptic
+sudo apt-get update
+sudo apt install synaptic -y
+
+# Install Google-Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
+
+# Install VLC
+sudo apt update
+sudo apt install vlc
+
+# Battery Saver
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt install powertop -y
+sudo powertop –calibrate
+
+sudo prime-select intel
+
+# Multitouch
+sudo apt-get update
+sudo apt install curl -y
+
+sudo apt-get upgrade -y
+
